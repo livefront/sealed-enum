c6882e7f43e566e543d9c4d054c273153d8ce69a
Alex Vanyo <alex@livefront.com>
2021-05-11 10:24:59 -0500

    Update Dokka to 1.4.32

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val detekt = "1.15.0"
-    const val dokka = "1.4.30"
+    const val dokka = "1.4.32"
     const val kotlin = "1.4.0"
     const val kotlinBinaryCompatibilityValidator = "0.4.0"
 }

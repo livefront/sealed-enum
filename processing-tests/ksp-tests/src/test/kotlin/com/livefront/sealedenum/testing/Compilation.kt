5ed2e6c2168d35682ad6be22651f507b02419337
Alex Vanyo <vanyoalex@gmail.com>
2021-05-16 17:24:19 -0500

    Update KSP to alpha08

--- processing-tests/ksp-tests/src/test/kotlin/com/livefront/sealedenum/testing/Compilation.kt
+++ processing-tests/ksp-tests/src/test/kotlin/com/livefront/sealedenum/testing/Compilation.kt
@@ -1,16 +1,16 @@
 package com.livefront.sealedenum.testing
 
-import com.livefront.sealedenum.internal.ksp.SealedEnumProcessor
+import com.livefront.sealedenum.internal.ksp.SealedEnumProcessorProvider
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
-import com.tschuchort.compiletesting.symbolProcessors
+import com.tschuchort.compiletesting.symbolProcessorProviders
 
 /**
- * Compiles the given [sourceFiles] with the application of [SealedEnumProcessor].
+ * Compiles the given [sourceFiles] with the application of [SealedEnumProcessorProvider].
  */
 internal fun compile(vararg sourceFiles: SourceFile): KotlinCompilation.Result =
     KotlinCompilation().apply {
         sources = sourceFiles.toList()
-        symbolProcessors = listOf(SealedEnumProcessor())
+        symbolProcessorProviders = listOf(SealedEnumProcessorProvider())
         inheritClassPath = true
     }.compile()

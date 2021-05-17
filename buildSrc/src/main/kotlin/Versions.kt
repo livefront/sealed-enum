9023b90fa302da8152b49ef17ea841c6118549d5
Alex Vanyo <vanyoalex@gmail.com>
2021-05-16 21:00:20 -0500

    Update detekt to 1.17.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val detekt = "1.17.0-RC2"
+    const val detekt = "1.17.0"
     const val dokka = "1.4.32"
     const val jacoco = "0.8.7"
     const val kotlin = "1.4.0"

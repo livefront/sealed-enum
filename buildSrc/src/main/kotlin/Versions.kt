55fc0a085cd27ac39d5b7ffa71b518d29dbaf757
Alex Vanyo <vanyoalex@gmail.com>
2021-05-16 21:06:23 -0500

    Update kotlin binary compatibility validator to 0.5.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,5 +3,5 @@ object Versions {
     const val dokka = "1.4.32"
     const val jacoco = "0.8.7"
     const val kotlin = "1.4.0"
-    const val kotlinBinaryCompatibilityValidator = "0.4.0"
+    const val kotlinBinaryCompatibilityValidator = "0.5.0"
 }

6b57749d59bc6ff6772561ee3afe86d3283c73da
Alex Vanyo <vanyoalex@gmail.com>
2021-05-26 21:31:52 -0500

    Update KSP to 1.0.0-beta01

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
     }
 
     plugins {
-        id("com.google.devtools.ksp") version "1.5.0-1.0.0-alpha10"
+        id("com.google.devtools.ksp") version "1.5.10-1.0.0-beta01"
     }
 }
 

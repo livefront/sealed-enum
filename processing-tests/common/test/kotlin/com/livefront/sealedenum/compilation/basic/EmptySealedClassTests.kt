991fda18a3996e63d0862d6b2ea0f5dc6a19fdf5
Alex Vanyo <vanyo@google.com>
2021-08-19 20:05:18 -0500

    Add runtimeSources

--- processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
+++ processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
@@ -1,30 +1,36 @@
 package com.livefront.sealedenum.compilation.basic
 
 import com.livefront.sealedenum.testing.assertCompiles
+import com.livefront.sealedenum.testing.assertGeneratedFileMatches
 import com.livefront.sealedenum.testing.compile
-import com.tschuchort.compiletesting.SourceFile
+import com.livefront.sealedenum.testing.getCommonSourceFile
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.nio.file.Paths
 
 class EmptySealedClassTests {
     @Test
-    fun `compilation generates correct code`() {
-        val paths = arrayOf("compilation", "basic", "EmptySealedClass.kt")
-        val result = compile(
-            SourceFile.fromPath(
-                Paths.get(
-                    "..",
-                    "common",
-                    "test",
-                    "kotlin",
-                    "com",
-                    "livefront",
-                    "sealedenum",
-                    *paths
-                ).toFile()
-            )
+    fun `empty sealed class`() {
+        assertEquals(emptyList<EmptySealedClass>(), EmptySealedClass.values)
+    }
+
+    @Test
+    fun `empty enum for sealed class`() {
+        assertEquals(
+            EmptySealedClass.values.map(EmptySealedClass::enum),
+            enumValues<EmptySealedClassEnum>().toList()
         )
+    }
+
+    @Test
+    fun `correct enum class`() {
+        assertEquals(EmptySealedClassEnum::class.java, EmptySealedClass.sealedEnum.enumClass)
+    }
+
+    @Test
+    fun `compilation generates correct code`() {
+        val result = compile(getCommonSourceFile("compilation", "basic", "EmptySealedClass.kt"))
 
         assertCompiles(result)
+        assertGeneratedFileMatches("EmptySealedClass_SealedEnum.kt", emptySealedClassGenerated, result)
     }
 }
--- processing-tests/ksp-tests/src/test/kotlin/com/livefront/sealedenum/testing/Compilation.kt
+++ processing-tests/ksp-tests/src/test/kotlin/com/livefront/sealedenum/testing/Compilation.kt
@@ -4,13 +4,23 @@ import com.livefront.sealedenum.internal.ksp.SealedEnumProcessorProvider
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.symbolProcessorProviders
+import java.io.File
+import java.nio.file.Paths
 
 /**
  * Compiles the given [sourceFiles] with the application of [SealedEnumProcessorProvider].
  */
 internal fun compile(vararg sourceFiles: SourceFile): KotlinCompilation.Result =
     KotlinCompilation().apply {
-        sources = sourceFiles.toList()
+        // Directly add all runtime sources to compilation, instead of inheriting the classpath
+        val runtimeSources =
+            Paths.get("..", "..", "runtime", "src", "main")
+                .toFile()
+                .walk()
+                .filter(File::isFile)
+                .map(SourceFile::fromPath)
+
+        sources = sourceFiles.toList() + runtimeSources
         symbolProcessorProviders = listOf(SealedEnumProcessorProvider())
         inheritClassPath = false
     }.compile()

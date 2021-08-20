a2c0e7111819beb35f8a5f60dc5eddc5b166089e
Alex Vanyo <vanyo@google.com>
2021-08-19 20:06:55 -0500

    Remove generated code check

--- processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
+++ processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
@@ -1,7 +1,6 @@
 package com.livefront.sealedenum.compilation.basic
 
 import com.livefront.sealedenum.testing.assertCompiles
-import com.livefront.sealedenum.testing.assertGeneratedFileMatches
 import com.livefront.sealedenum.testing.compile
 import com.livefront.sealedenum.testing.getCommonSourceFile
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -31,6 +30,5 @@ class EmptySealedClassTests {
         val result = compile(getCommonSourceFile("compilation", "basic", "EmptySealedClass.kt"))
 
         assertCompiles(result)
-        assertGeneratedFileMatches("EmptySealedClass_SealedEnum.kt", emptySealedClassGenerated, result)
     }
 }

16e37e030c5d75debe7723bd723577f1fdcaf2a5
Alex Vanyo <vanyo@google.com>
2021-08-19 20:14:21 -0500

    Inline again

--- processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
+++ processing-tests/common/test/kotlin/com/livefront/sealedenum/compilation/basic/EmptySealedClassTests.kt
@@ -2,14 +2,29 @@ package com.livefront.sealedenum.compilation.basic
 
 import com.livefront.sealedenum.testing.assertCompiles
 import com.livefront.sealedenum.testing.compile
-import com.livefront.sealedenum.testing.getCommonSourceFile
+import com.tschuchort.compiletesting.SourceFile
 import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 class EmptySealedClassTests {
 
     @Test
     fun `compilation generates correct code`() {
-        val result = compile(getCommonSourceFile("compilation", "basic", "EmptySealedClass.kt"))
+        val paths = arrayOf("compilation", "basic", "EmptySealedClass.kt")
+        val result = compile(
+            SourceFile.fromPath(
+                Paths.get(
+                    "..",
+                    "common",
+                    "test",
+                    "kotlin",
+                    "com",
+                    "livefront",
+                    "sealedenum",
+                    *paths
+                ).toFile()
+            )
+        )
 
         assertCompiles(result)
     }

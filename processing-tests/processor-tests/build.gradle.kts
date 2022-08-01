9b49a5b8a3e335a100c9cf384beeb6febe664db3
Alex Vanyo <vanyo@google.com>
2022-07-31 23:25:31 -0700

    Add testing on JDK 16 and 17

--- processing-tests/processor-tests/build.gradle.kts
+++ processing-tests/processor-tests/build.gradle.kts
@@ -38,3 +38,21 @@ dependencies {
     testImplementation(projects.processor)
     kaptTest(projects.processor)
 }
+
+// See https://github.com/tschuchortdev/kotlin-compile-testing#java-16-compatibility
+if (JavaVersion.current() >= JavaVersion.VERSION_16) {
+    tasks.withType<Test>().all {
+        jvmArgs(
+            "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        )
+    }
+}
